f471b7a704e2bb2b0b0eecf2861038e66f68dae9
Artassinxe <anthonychaigneau@live.fr>
2024-03-07 19:08:42 +0100

    Question 4.d

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,8 +1,11 @@
 package utils;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.ListIterator;
+import java.util.Objects;
+import java.util.Collections;
 
 public class Utils {
 	
@@ -39,5 +42,34 @@ public class Utils {
 		return liste;
 	}
 	
+	public static <E> Boolean verifierMelange(List<E> liste1, List<E> liste2) {
+		if (liste1.size() != liste2.size() || liste1.isEmpty() || liste2.isEmpty())
+			return false;
+		List<E> knownObj = new ArrayList<>();
+		for (ListIterator<E> iterator = liste1.listIterator();iterator.hasNext();) {
+			E obj = iterator.next();
+			if (!knownObj.contains(obj)) {
+				if (Collections.frequency(liste1, obj) != Collections.frequency(liste2, obj)) 
+					return false;
+				else knownObj.add(obj);
+			}
+		}
+		return true;
+	}
+	
+	public static <E> List<E> rassembler(List<E> liste) {
+	    List<E> newList = new ArrayList<>();
+	    int lastIndex = 0;
+	    for (E obj : liste) {
+	    	lastIndex = newList.lastIndexOf(obj);
+	    	if (lastIndex!=-1) newList.add(lastIndex, obj);
+	    	else newList.add(obj);
+	    }
+	    return newList;
+	}
+	
+	public static <E> Boolean verifierRassemblement(List<E> liste) {
+		return true;
+	}
 	
 }
